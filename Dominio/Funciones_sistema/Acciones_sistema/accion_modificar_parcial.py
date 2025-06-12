1d5a7cdcd6bcdceb1963a2e58ce69696663e60c8
Nicolas Galetto <nicomgaletto@gmail.com>
2025-06-11 21:28:23 -0300

    mas acciones

--- Dominio/Funciones_sistema/Acciones_sistema/accion_modificar_parcial.py
+++ Dominio/Funciones_sistema/Acciones_sistema/accion_modificar_parcial.py
@@ -1,4 +1,7 @@
 from Dominio.Funciones_sistema.Acciones_sistema.accion import Accion
+from Dominio.Funciones_sistema.Acciones_sistema.accion_modificar import Modificar
+from Dominio.Funciones_sistema.Acciones_sistema.accion_modificar_valor_nota import Modificar_Valor_Nota
+from Dominio.Funciones_sistema.Acciones_sistema.accion_modificar_valor_recuperatorio import Modificar_Valor_Recuperatorio
 
 class Modificar_Parcial(Accion):
     def __init__(self, main, materia, nota):
@@ -12,10 +15,10 @@ class Modificar_Parcial(Accion):
         }
 
     def valor_nota_seleccionado(self):
-        self.main.accion = ValorNotaSeleccionado(self.main, self.materia_seleccionada, self.parcial_seleccionado)
+        self.main.accion = Modificar_Valor_Nota(self.main, self.materia_seleccionada, self.parcial_seleccionado)
 
     def valor_recuperatorio_seleccionado(self):
-        self.main.accion = ValorRecuperatorioSeleccionado(self.main, self.materia_seleccionada, self.parcial_seleccionado)
+        self.main.accion = Modificar_Valor_Recuperatorio(self.main, self.materia_seleccionada, self.parcial_seleccionado)
 
     def volver(self):
         self.main.accion = Modificar(self.main, self.materia_seleccionada)
---
+++ Dominio/Funciones_sistema/Acciones_sistema/accion_modificar_valor_nota.py
@@ -0,0 +1,23 @@
+from Dominio.Funciones_sistema.Acciones_sistema.accion import Accion
+from Dominio.Funciones_sistema.Acciones_sistema.accion_seleccionar import Seleccionar
+from Dominio.Funciones_sistema.Acciones_sistema.accion_modificar_parcial import Modificar_Parcial
+
+class Modificar_Valor_Nota(Accion):
+    def __init__(self, main, materia, id_nota):
+        super().__init__(main)
+        self.materia_seleccionada = materia
+        self.id_nota = id_nota
+
+    def cambiar_a_seleccionar(self):
+        self.main.accion = Seleccionar(self.main, self.materia_seleccionada)
+
+    def volver(self):
+        self.main.accion = Modificar_Parcial(self.main, self.materia_seleccionada, self.id_nota)
+
+    def realizar_accion(self):
+        valor = self.main.cli.obtener_dato("Nota del parcial (X = Volver): ")
+        if valor.upper() != "X":
+            self.main.persistencia.modificar_parcial(self.id_nota, "valor_nota", valor)
+            self.cambiar_a_seleccionar()
+        else:
+            self.volver()
